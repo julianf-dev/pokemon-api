80a9c6a2541bb54cd315aa7744612ff04bbcb108
JulianF <juliandavidbf@gmail.com>
2022-05-26 13:51:42 -0500

    @feat: update filters

--- src/app.js
+++ src/app.js
@@ -1,128 +1,120 @@
 const appNode = document.querySelector('.cards-container')
 const search = document.querySelector("#search");
+const pokemonContainer = document.querySelector(".cards-container");
 
-const paintPokemon = (container, pokemon) => {
-    container.innerHTML = " ";
+
+const divNode = ({
+    property: characteristic,
+    nameProperty: paragraph,
+
+}) => {
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    span.className = "pokemon-info-detail";
+    span.textContent = characteristic;
+    const p = document.createElement('p');
+    p.textContent = paragraph;
+    div.append(span, p)
+
+    return div;
+}
+
+function paintPokemon(pokemon) {
+
 
     const allItems = [];
 
+    const article = document.createElement('article')
+    article.classList.add('card-pokemon')
+
     const title = document.createElement('h2');
     title.textContent = pokemon.name.toUpperCase();
+
     const image = document.createElement('img');
-    image.src = pokemon.image;
+    image.src = pokemon.sprites.other.dream_world.front_default;
     image.alt = pokemon.name;
 
     const info = document.createElement('div');
     info.className = 'pokemon-info'
 
     //Altura
-    const height = document.createElement('div');
-    const heightSpan = document.createElement('span');
-    heightSpan.className = "pokemon-info-detail";
-    heightSpan.textContent = pokemon.height;
-    const heightParagraph = document.createElement('p');
-    heightParagraph.textContent = 'Height';
-    height.append(heightSpan, heightParagraph);
+    const height = divNode({
+        property: pokemon.height,
+        nameProperty: 'Height'
+    });
 
-    //Ability
-    const ability = document.createElement('div');
-    const abilitySpan = document.createElement('span');
-    abilitySpan.className = "pokemon-info-detail";
-    abilitySpan.textContent = pokemon.ability;
-    const abilityParagraph = document.createElement('p');
-    abilityParagraph.textContent = 'Ability';
-    ability.append(abilitySpan, abilityParagraph);
+    const ability = divNode({
+        property: pokemon.abilities[0].ability.name,
+        nameProperty: 'Ability'
+    });
 
     //type
-
-    const type = document.createElement('div');
-    const typeSpan = document.createElement('span');
-    typeSpan.className = "pokemon-info-detail";
-    typeSpan.textContent = pokemon.type;
-    const typeParagraph = document.createElement('p');
-    typeParagraph.textContent = 'Type';
-    type.append(typeSpan, typeParagraph);
+    const type = divNode({
+        property: pokemon.types[0].type.name,
+        nameProperty: 'Type'
+    })
 
     //container 
     info.append(height, ability, type)
 
     //article container
-    container.append(image, title, info)
-    allItems.push(container)
+    article.append(image, title, info)
 
-    return allItems
-    /*   return (
-            `
-            <img src="${pokemon.image}"  alt=${pokemon.name} loading="lazy">
-            <h2>${pokemon.name}</h2>
-            <div class="pokemon-info" >
-                <div>
-                    <span class="pokemon-info-detail">${pokemon.height}</span>
-                    <p>Height</p>
-                </div>
-                <div>
-                    <span class="pokemon-info-detail">${pokemon.ability}</span>
-                    <p>Ability</p>
-                </div>
-                <div>
-                    <span class="pokemon-info-detail">${pokemon.type}</span>
-                    <p>Type</p>
-                </div>
-            </div>
-            `
-        ) */
+    allItems.push(article)
+    pokemonContainer.append(...allItems)
+
+    return pokemonContainer
 }
 
 const getDataAleatory = async () => {
     try {
-        const pokemonContainer = document.querySelector(".card-pokemon");
         const idPokemon = Math.round(Math.random() * (1, 100));
-        const API = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`
+        /* const API = `https://pokeapi.co/api/v2/pokemon/${idPokemon}` */
+        const API = `https://pokeapi.co/api/v2/pokemon/?limit=10`
         const res = await fetch(API);
         const data = await res.json();
-        const pokemons = {
-            name: data.name,
-            image: data.sprites.other.dream_world.front_default,
-            height: data.height,
-            type: data.types[0].type.name,
-            ability: data.abilities[0].ability.name,
-        } 
-        paintPokemon(pokemonContainer,pokemons) 
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const getData = async (busqueda) => {
-    try {
-        const API = `https://pokeapi.co/api/v2/pokemon/?limit=500`
-        const res = await fetch(API);
-        const data = await res.json();
-        const results = data.results;
-        results.filter(item =>  {
-            if ( item.name.toLowerCase().includes(busqueda.toLowerCase())) {
-                pokemonData(item.url)
-            }
+        const pokemons = data.results;
+        pokemons.map(pokemon => {
+            urlPokemons(pokemon.url);
         })
     } catch (error) {
         console.log(error);
     }
 }
 
-const  pokemonData = async (response) => {
+async function urlPokemons(pokemons) {
     try {
-        const pokemonContainer = document.querySelector(".card-pokemon");
+        const res = await fetch(pokemons);
+        const data = await res.json();
+        paintPokemon(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const getData = async (busqueda) => {
+    try{
+        const API = `https://pokeapi.co/api/v2/pokemon/?limit=500`
+        const res = await fetch(API);
+        const data = await res.json();
+        const results = data.results;
+        results.filter(item => {
+            if (item.name.toLowerCase().includes(busqueda.toLowerCase())) {
+                pokemonData(item.url)
+            }
+        })
+    }catch (error){
+        console.log(error);
+    }
+
+}
+
+const pokemonData = async (response) => {
+    try {
         const res = await fetch(response)
         const data = await res.json()
-        const pokemon = {
-            name: data.name,
-            image: data.sprites.other.dream_world.front_default,
-            height: data.height,
-            type: data.types[0].type.name,
-            ability: data.abilities[0].ability.name,
-        } 
-        paintPokemon(pokemonContainer, pokemon)
+        paintPokemon(data);
 
     } catch (error) {
         console.log(error);
@@ -131,10 +123,12 @@ const  pokemonData = async (response) => {
 
 search.addEventListener('change', (e) => {
     getData(e.target.value)
-    search.textContent= " "
 })
 
 
 const geratePokemon = document.getElementById("generate-pokemon");
-geratePokemon.addEventListener('click',getDataAleatory);
-document.addEventListener('DOMContentLoaded',getDataAleatory); 
+geratePokemon.addEventListener('click', () => {
+    pokemonContainer.innerHTML = " ";
+    getDataAleatory()
+});
+document.addEventListener('DOMContentLoaded', getDataAleatory); 
