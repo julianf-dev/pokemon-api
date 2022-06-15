efc1e8d7e43491ce91b0bac900a8f899288b120f
JulianF <juliandavidbf@gmail.com>
2022-06-15 10:19:18 -0500

    @fix: Fixing routes

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: '/pokemon-api/',
+    build: {
+        outDir: "dist",
+        assetsDir: "assets"
+    },
+    plugins: []
 })
